1aad4d6bcf5027fb931e519e9728eb6cf5e2cf12
zentauro <pedrogomezmartin1999@gmail.com>
2019-10-31 01:52:15 +0100

    Front Tables page initial imports

---
+++ frontend/src/pages/Tables.tsx
@@ -0,0 +1,6 @@
+import React  from 'react';
+import Images from '../components/Images';
+import styled from 'styled-components';
+
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import { colors } from '../config/colors';
